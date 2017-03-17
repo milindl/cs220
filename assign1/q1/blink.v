a9b0f4f4342086cf36c8618054e7a89fda49d06b
Yash Srivastav <admin@yashsriv.org>
2017-03-17 12:11:50 +0530

    Updated Program 1

--- assign1/q1/blink.v
+++ assign1/q1/blink.v
@@ -1,41 +1,40 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    11:27:32 03/06/2017 
-// Design Name: 
-// Module Name:    blink 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 1ps
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: IIT Kanpur
+// Engineer:
+//   Milind Luthra
+//   Subhdeep Saha
+//   Yash Srivastav
+//
+// Create Date:    11:48:05 03/17/2017
+// Design Name:
+// Module Name:    blink
+// Project Name: Q1
+// Target Devices: Xilinx Spartan-3e Starter
+// Description:
+//   Blinks LED periodically
+//
+//
+// Revision:
+// Revision 0.01 - File Created
+//
+//////////////////////////////////////////////////////////////////////////////////
 module blink(
-	input wire clk,	// clock signal
-	output reg ledpin	// LED pin
+             input wire clk,
+             output reg ledpin
     );
 
-	initial
-		ledpin = 0;	
+   initial
+     ledpin = 0;
 
-	// internal variable
-	reg [25:0] counter = 50_000_000;	// 26 bit variable
+   reg [25:0]           counter = 50_000_000;
 
-	always @(posedge clk)
-		if (counter == 0) begin			// at 1 second
-			counter <= 50_000_000;		// reset counter
-			ledpin <= !ledpin;		// invert ledpin
-		end else begin
-			counter <= counter - 1;		// decrease
-		end
+   always @(posedge clk)
+     if (counter == 0) begin
+        counter <= 50_000_000;
+        ledpin <= !ledpin;
+     end else begin
+        counter <= counter - 1;
+     end
 
 endmodule
