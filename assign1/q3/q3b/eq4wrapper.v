2901c79e70e94a3d646b0c4da5f7cf79543a2870
Yash Srivastav <yashsriv@csews79.cse.iitk.ac.in>
2017-03-20 16:06:54 +0530

    Update from Lab

--- assign1/q3/q3b/eq4wrapper.v
+++ assign1/q3/q3b/eq4wrapper.v
@@ -1,23 +1,23 @@
 module eq4wrapper
   (
+   input wire clk,
    input wire [3:0] sw,
    input wire       pb1, pb2,
    output wire      ledpin
    );
+	reg [3:0] a = 4'b0000;
+	reg [3:0] b = 4'b0000;
+	
 
-   reg              a = 4'b0000;
-   reg              b = 4'b0000;
-
-   eq4 eq_unit (.a(a), .b(b), .aeqb(ledpin));
-
-   always @(pb1)
-     begin
-        a <= sw;
-     end
-
-   always @(pb2)
-     begin
-        b <= sw;
-     end
+	
+	always @(posedge clk)
+	begin
+		if (pb1 == 1)
+			a = sw;
+		if (pb2 == 1)
+			b = sw;
+	end
+	
+	eq4 eq_unit (.a(a), .b(b), .aeqb(ledpin));
 
 endmodule
